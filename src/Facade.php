88f02191aa0ef6f162ef84e9e32dd517fe2ea4d5
Xiaohui Lam <xiaohuilam@users.noreply.github.com>
2019-09-08 12:28:47 +0800

    修复php报错

--- src/Facade.php
+++ src/Facade.php
@@ -1,9 +1,9 @@
 <?php
 namespace Xorpay;
 
-use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Facade as BaseFacade;
 
-class Facade
+class Facade extends BaseFacade
 {
     /**
      * Get the registered name of the component.
